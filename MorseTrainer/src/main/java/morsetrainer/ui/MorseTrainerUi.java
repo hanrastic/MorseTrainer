8a1029cd4a898c69e91a29e8c89e1f2d2e07d58a
joonas <joonas@LAPTOP-COH56K56>
2020-11-18 11:20:36 +0200

    GUI:n koodailua

--- MorseTrainer/src/main/java/morsetrainer/ui/MorseTrainerUi.java
+++ MorseTrainer/src/main/java/morsetrainer/ui/MorseTrainerUi.java
@@ -10,6 +10,7 @@ import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
@@ -31,63 +32,73 @@ public class MorseTrainerUi extends Application {
         primaryStage.setTitle("MorseTrainer");
             //SceneTitle Gridpane (1,0)
             Text introTextField = new Text("MorseTrainer 2.0");
-            introTextField.setFont(Font.font("Verdana", FontWeight.BOLD, 40));
+            introTextField.setFont(Font.font("Verdana", FontWeight.BOLD, 20));
+            
                 //Train button Gridpane(2,1)
                 Button trainButton = new Button("Train");
-                trainButton.setPadding(new Insets(20,30,20,30));
-                    //VBox for logging in and create new account Gridpane (0,1)
-                    TextField username = new TextField("username");
-                    TextField password = new TextField("password");
-                    Button logIn = new Button("logIn");
-                    Button createAccount = new Button ("create account");
+                trainButton.setPadding(new Insets(23,30,23,30));
+                    //Info pop-up button Gridpane (2,2)
+                    Button infoButton = new Button ("Info");
+                    infoButton.setPadding(new Insets (20,32,20,32));
+                            
+                        //vBox for buttons
+                        VBox buttonColumn = new VBox();
+                        buttonColumn.setSpacing(10);
+                        buttonColumn.setPadding(new Insets(20,30,20,30));
+                        buttonColumn.getChildren().add(infoButton);
+                        buttonColumn.getChildren().add(trainButton);
+                            //VBox for logging in and create new account Gridpane (0,1)
+                            TextField username = new TextField();
+                            username.setPromptText("username");
+                            PasswordField password = new PasswordField();
+                            password.setPromptText("password");
+                            Button logIn = new Button("log In");
+                            Button createAccount = new Button ("create account");
 
-                    VBox signInColumn = new VBox();
-                    signInColumn.setSpacing(10);
-                    signInColumn.setPadding(new Insets(20,30,20,30));
-                    signInColumn.getChildren().add(username);
-                    signInColumn.getChildren().add(password);
-                    signInColumn.getChildren().add(logIn);
-                    signInColumn.getChildren().add(createAccount);                   
-                        //Info pop-up button Gridpane (2,2)
-                        Button popupButton = new Button ("info");
-                        popupButton.setPadding(new Insets (5,30,5,30));
-          
-                       
+                            VBox signInColumn = new VBox();
+                            signInColumn.setSpacing(10);
+                            signInColumn.setPadding(new Insets(20,30,20,30));
+                            signInColumn.getChildren().add(username);
+                            signInColumn.getChildren().add(password);
+                            signInColumn.getChildren().add(logIn);
+                            signInColumn.getChildren().add(createAccount);                   
+                     
         GridPane layout = new GridPane();
         
-        //layout.setSize(600, 300);
+       
                                     //Top, Right, Bottom, Left
         layout.setPadding(new Insets(5,30,30,30));
         layout.setAlignment(Pos.CENTER);
-        
+        GridPane.setMargin(introTextField, new Insets(0,0,0,32));
         layout.add(introTextField,0,0);
-        layout.add(trainButton,0,1);
+        layout.add(buttonColumn,0,1);
         layout.add(signInColumn, 1, 1);
-        layout.add(popupButton, 0, 2);
         
         Scene mainScene = new Scene(layout);
         
         
+        
+        //Info Scene
+        String text = "Welcome to Morsetrainer! \n "
+                    + "\nThis application is for learning Morse code. Application offers an training mode where you can get to know morse as well as practice converting letters. \n"
+                    + "\nYou can also log in or create account in the main menu. Do this to keep track of your progress. ";
+        Text infoText = new Text(text);
+        Button backToPrimaryButton = new Button("Back to main menu");
+        
+        GridPane infoLayout = new GridPane();
+        infoLayout.setPadding(new Insets(20,20,20,20));
+        infoLayout.setVgap(30);
+        infoLayout.add(infoText, 0, 0);
+        infoLayout.add(backToPrimaryButton, 0, 2);
+        Scene infoScene = new Scene(infoLayout);       
+        infoButton.setOnAction(e -> primaryStage.setScene(infoScene));
+        backToPrimaryButton.setOnAction(e -> primaryStage.setScene(mainScene));
+        
+        
+        
+        
         primaryStage.setScene(mainScene);
-        primaryStage.show();
-        
-        //login/signup scene
-//        
-//        GridPane grid = new GridPane();
-//        grid.setAlignment(Pos.CENTER);
-//        grid.setHgap(10);
-//        grid.setVgap(10);
-//        grid.setPadding(new Insets(25, 25, 25, 25));
-//
-//        Scene login_createAccountScene = new Scene(grid, 300, 275);
-//        primaryStage.setScene(scene);
-//
-//        Text sceneTitle = new Text("Here you can log in or create a new account!");
-//        sceneTitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
-//        grid.add(sceneTitle, 0, 1);
-        
-        
-        
+        primaryStage.show();   
     }
 
     
